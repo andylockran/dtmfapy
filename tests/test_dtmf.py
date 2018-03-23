2088102206cdd05073c8ed72b169347d7769fb80
Andy Loughran <aloughran@deloitte.co.uk>
2018-03-23 09:32:28 +0000

    UpdateD

--- tests/test_dtmf.py
+++ tests/test_dtmf.py
@@ -1,26 +1,45 @@
 import unittest
-
+import os
 from dtmfa import parse
+import re
 
 class TestDTMF(unittest.TestCase):
 
-    def test_dtmf_tones(self):
-        """Sample DTMF tone sends a string, DTMF should detect string"""
-        input = "0123456789p*ABCD"
-        sample = "/Users/aloughran/Code/Hackathon/audio/output16.wav"
-        result = parse.method1(sample)
-        assert result == input
+    # def test_dtmf_tones(self):
+    #     """Sample DTMF tone sends a string, DTMF should detect string"""
+    #     input = "0123456789p*ABCD"
+    #     sample = "/Users/aloughran/Code/Hackathon/audio/output16.wav"
+    #     result = parse.method1(sample)
+    #     assert result == input
 
-    def test_dtmf_tone(self):
-        """Sample DTMF tone sends a string, DTMF should detect string"""
-        input = "708078"
-        sample = "/Users/aloughran/Code/Hackathon/audio/output.wav"
-        result = parse.method5(sample)
-        assert result == input
+    # def test_dtmf_tone(self):
+    #     """Sample DTMF tone sends a string, DTMF should detect string"""
+    #     input = "708078"
+    #     sample = "/Users/aloughran/Code/Hackathon/audio/708078.wav"
+    #     result = parse.method5(sample)
+    #     assert result == input
 
-    def test_dtmf_tone_broken(self):
-        """Sample DTMF tone sends a string, DTMF should detect string"""
-        input = "708078"
-        sample = "/Users/aloughran/Code/Hackathon/audio/brokenoutput.wav"
-        result = parse.method5(sample)
-        assert result == input
+    # def test_dtmf_tone_broken(self):
+    #     """Sample DTMF tone sends a string, DTMF should detect string"""
+    #     input = "708078"
+    #     sample = "/Users/aloughran/Code/Hackathon/audio/brokenoutput.wav"
+    #     result = parse.method5(sample)
+    #     assert result == input
+
+    # def test_dtmf_tone_309738(self):
+    #     """Sample DTMF tone sends a string, DTMF should detect string"""
+    #     input = "309738"
+    #     sample = "/Users/aloughran/Code/Hackathon/audio/309738.wav"
+    #     result = parse.method5(sample)
+    #     assert result == input
+
+    def test_all_audiofiles(self):
+        """Tests all audio files in the audio folder"""
+        cwd = os.getcwd()
+        print(cwd)
+        for filename in os.listdir('audio/'):
+            expected = re.split('.', filename)[0]
+            testfile = cwd + "/audio/" + filename
+            result = parse.method5(testfile)
+            asseqrt expected==result
+        
